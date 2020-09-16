c47c4952435b1d0ec51f52e83166e560aaebb42d
cyx20101005 <cyx20101005@gmail.com>
2020-09-16 01:57:45 -0700

    Update ProfileFragment, set visitor view profile layout, set VisitorViewProfileFragment

---
+++ app/src/main/java/com/flagcamp/secondhands/ui/profile/VisitorViewProfileFragment.java
@@ -0,0 +1,48 @@
+package com.flagcamp.secondhands.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.flagcamp.secondhands.R;
+import com.flagcamp.secondhands.databinding.FragmentVisitorViewProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+
+public class VisitorViewProfileFragment extends Fragment {
+
+    private FragmentVisitorViewProfileBinding binding;
+
+    public VisitorViewProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_visitor_view_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // info
+
+        binding.visitorViewProfileChatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // link to new Chat Page
+            }
+        });
+    }
+}
