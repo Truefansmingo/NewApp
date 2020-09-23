02009fb81bdbbee41b6b7eff8a128d161384cef8
losvhesy <iamlinshu@gmail.com>
2020-09-22 22:25:28 -0400

    Revert "Revert "Revert "reset the master file"""
    This reverts commit f2657a7587015719c58002ce6585a19ff69f87ee.

---
+++ app/src/main/java/com/flagcamp/secondhands/MainActivity.java
@@ -0,0 +1,41 @@
+package com.flagcamp.secondhands;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigator;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.flagcamp.secondhands.navigation.KeepStateNavigator;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private NavController navController;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+
+        Navigator navigator =
+                new KeepStateNavigator(this, navHostFragment.getChildFragmentManager(), R.id.nav_host_fragment);
+        navController.getNavigatorProvider().addNavigator(navigator);
+
+        navController.setGraph(R.navigation.nav_graph);
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+     @Override
+        public boolean onSupportNavigateUp() {
+            return navController.navigateUp();
+         }
+}
+
+
